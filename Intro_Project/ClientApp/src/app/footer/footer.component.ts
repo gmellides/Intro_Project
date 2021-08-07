f2db5a9ed37ae60708fe648b65fe5a5fc5ad30ca
Gabriel Mellides <gabriel_mellides@outlook.com>
2021-08-07 20:21:55 +0300

    Front end changes

--- Intro_Project/ClientApp/src/app/footer/footer.component.ts
+++ Intro_Project/ClientApp/src/app/footer/footer.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  public year;
 
-  constructor() { }
+  constructor() { 
+    this.year = new Date().getFullYear()
+  }
 
   ngOnInit() {
   }
