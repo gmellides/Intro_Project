2403da5282079c1faaa4a4b50335504d6344d98b
Gabriel Mellides <gabriel_mellides@outlook.com>
2021-08-30 14:37:04 +0300

    Unit tests for User Service class.

---
+++ Intro.Tests/UserAPITests.cs
@@ -0,0 +1,109 @@
+using Intro.Models.DTO;
+using Intro.Models.Model;
+using Intro.WebApi.Repositories;
+using Intro.WebApi.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Intro.Tests
+{
+    [TestClass]
+    public class UserAPITests
+    {
+        public UserServices _userService;
+        public Mock<IRepository<UserTitle>> _userTitleRepository;
+        public Mock<IRepository<UserType>> _userTypeRepository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _userTitleRepository = new Mock<IRepository<UserTitle>>();
+            _userTypeRepository = new Mock<IRepository<UserType>>();
+            _userService = new UserServices(_userTitleRepository.Object, _userTypeRepository.Object);
+        }
+
+
+
+        [TestMethod]
+        public void AssertUserIsDeleted_Success()
+        {
+            #region Create User Entity
+            User user = new User
+            {
+                Id = 1,
+                Name = "John",
+                Surname = "Doe",
+                BirthDate = System.DateTime.Now,
+                EmailAddress = "test@test.gr",
+                UserTitle = new UserTitle
+                {
+                    Id=1,
+                    Description = "Title Description",
+                },
+                UserType = new UserType
+                {
+                    Id = 1,
+                    Description = "Type Description",
+                    Code = "TS"
+                },
+                IsActive = true
+            }; 
+            #endregion
+
+            user = _userService.DeleteUser(user);
+
+            Assert.AreEqual(user.IsActive, false);
+        }
+
+        [TestMethod]
+        public void TestEditUser_assertSuccess()
+        {
+            #region Create User Entity and DTO
+            User user = new User
+            {
+                Id = 1,
+                Name = "John",
+                Surname = "Doe",
+                BirthDate = System.DateTime.Now,
+                EmailAddress = "test@test.gr",
+                UserTitle = new UserTitle
+                {
+                    Id = 1,
+                    Description = "Title Description",
+                },
+                UserType = new UserType
+                {
+                    Id = 1,
+                    Description = "Type Description",
+                    Code = "TS"
+                },
+                IsActive = true
+            };
+
+            UserDTO dto = new UserDTO
+            {
+                Id = 1,
+                Name = "John",
+                Surname = "Doe",
+                BirthDate = System.DateTime.Now,
+                EmailAddress = "test@mail.gr",
+                UserTitle = new UserTitleDTO
+                {
+                    Description = "Title Description",
+                },
+                UserType = new UsertTypeDTO
+                {
+                    Description = "Type Description",
+                    Code = "TS"
+                },
+            };
+            #endregion
+
+            user = _userService.EditUserAction(user, dto);
+
+            Assert.AreEqual(user.Name, dto.Name);
+            Assert.AreEqual(user.EmailAddress, dto.EmailAddress);
+        }
+
+    }
+}
